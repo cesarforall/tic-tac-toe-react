472cb6cbc9ef3ab7a06134fc67960ca6083de828
César Almeida <cesaralmeida89@gmail.com>
2023-03-28 18:32:07 +0200

    :construction: add Square to App

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/styles.css';
+
+import App from './components/App.jsx';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
