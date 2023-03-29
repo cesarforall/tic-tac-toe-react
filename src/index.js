21f91554e4b170f2d3671f11dcf55a2842f0d013
César Almeida <cesaralmeida89@gmail.com>
2023-03-29 19:03:40 +0200

    :hammer: add StrictMode to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/styles.css';
 
 import App from './components/App.jsx';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
